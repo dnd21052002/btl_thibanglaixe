4d66fc976d3ae9bd6c8cf10a41c56065f41b142a
buivanphu <buivanphu62thnb@gmail.com>
2023-06-20 16:24:07 +0700

    update

---
+++ app/src/main/java/com/example/btl_thibanglaixe/Activities/LyThuyetActivity.java
@@ -0,0 +1,14 @@
+package com.example.btl_thibanglaixe.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.btl_thibanglaixe.R;
+
+public class LyThuyetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ly_thuyet);
+    }
+}
